f3472f8f2ef9da50de8c2d2e9f56974acc9048d0
saimomislam26 <saimomislam16@gmail.com>
2024-09-12 14:56:45 +0600

    All api integrated

--- server/app.js
+++ server/app.js
@@ -10,17 +10,13 @@ const app = express()
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    // console.log(process.env.ORIGIN);
     res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
-    // res.setHeader('Content-Security-Policy', 'script-src http://localhost:3000')
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, delete');
 
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization,Accept,filename');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
