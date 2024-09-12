18421a0fe8bd20ee8c8a0a09ebe1d60ee90f6a18
saimomislam26 <saimomislam16@gmail.com>
2024-09-12 15:50:35 +0600

    production ready server setup

--- client/src/Components/shared/Navbar.jsx
+++ client/src/Components/shared/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">Navbar</Link>
+                    <Link className="navbar-brand" to="/">Whiteboard App</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
--- server/app.js
+++ server/app.js
@@ -2,30 +2,40 @@ const express = require('express')
 const drawingRouter = require('./routers/drawingRouter')
 const bodyParser = require("body-parser");
 const dotenv = require('dotenv')
+const path = require("path");
 
 dotenv.config()
 
 const app = express()
 
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, delete');
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, delete');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization,Accept,filename');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization,Accept,filename');
 
-    res.setHeader('Access-Control-Allow-Credentials', true);
+//     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
+
+// Serve static files from the React app build folder
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.use('/api/drawing', drawingRouter)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
+
 
 module.exports = app
