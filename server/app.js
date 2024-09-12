b5b928d45a0901e4b13f7bc969c1e520ce1fec4b
saimomislam26 <saimomislam16@gmail.com>
2024-09-12 15:59:11 +0600

    production ready server setup

--- server/app.js
+++ server/app.js
@@ -8,21 +8,21 @@ dotenv.config()
 
 const app = express()
 
-// app.use(function (req, res, next) {
+app.use(function (req, res, next) {
 
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, delete');
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, delete');
 
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization,Accept,filename');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization,Accept,filename');
 
-//     res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
-//     // Pass to next layer of middleware
-//     next();
-// });
+    // Pass to next layer of middleware
+    next();
+});
 
 // Serve static files from the React app build folder
 app.use(express.static(path.join(__dirname, "../client/dist")));
